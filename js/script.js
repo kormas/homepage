45213d07203eb2841f31dd0506e50e45bda28637
Kornel Masnica <kornel.masnica@gmail.com>
2022-12-16 19:24:09 +0100

    modified code to functions

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
-console.log("Hello there!");
+{
+    const welcome = () => {
+        console.log("Hello there!");
+    };
+    
+    const toggleBackground = () => {
+        let body = document.querySelector(".js-body");
+        let theme = document.querySelector(".js-theme");
 
-let buttonElement = document.querySelector(".js-button");
-let body = document.querySelector(".js-body");
-let theme = document.querySelector(".js-theme");
+        body.classList.toggle("body--dark")
+        theme.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
+    };
+    
+    const init = () => {
+        const buttonElement = document.querySelector(".js-button");
+
+        buttonElement.addEventListener("click", toggleBackground);
+
+        welcome();
+    }
+}
 
 let howMuch = document.querySelector(".js-taskHowMuch").innerText;
 let ruler = document.querySelector(".js-taskRuler");
-
-buttonElement.addEventListener("click", () => {
-    body.classList.toggle("body--dark")
-    theme.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
-});
-
 switch (howMuch) {
     case "10":
         ruler.innerText = "Mieszko I";
