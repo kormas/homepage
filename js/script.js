096a6b7ac74156d7647e584c31680533ac7cdc59
Kornel Masnica <kornel.masnica@gmail.com>
2022-12-16 19:37:58 +0100

    modified code to functions v.3

--- js/script.js
+++ js/script.js
@@ -11,38 +11,42 @@
         theme.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
     };
     
+    const whichRuler = () => {
+        let howMuch = document.querySelector(".js-taskHowMuch").innerText;
+        let ruler = document.querySelector(".js-taskRuler");
+
+        switch (howMuch) {
+            case "10":
+                ruler.innerText = "Mieszko I";
+                break;
+            case "20":
+                ruler.innerText = "Bolesław I Chrobry";
+                break;
+            case "50":
+                ruler.innerText = "Kazimierz III Wielki";
+                break;
+            case "100":
+                ruler.innerText = "Władysław II Jagiełło";
+                break;
+            case "200":
+                ruler.innerText = "Zygmunt I Stary";
+                break;
+            case "500":
+                ruler.innerText = "Jan III Sobieski";
+                break;
+            default:
+                ruler.innerText = "Taki banknot nie istnieje";
+        };
+    };
+
     const init = () => {
         const buttonElement = document.querySelector(".js-button");
 
         buttonElement.addEventListener("click", toggleBackground);
 
         welcome();
+        whichRuler();
     }
 
     init();
 }
-
-let howMuch = document.querySelector(".js-taskHowMuch").innerText;
-let ruler = document.querySelector(".js-taskRuler");
-switch (howMuch) {
-    case "10":
-        ruler.innerText = "Mieszko I";
-        break;
-    case "20":
-        ruler.innerText = "Bolesław I Chrobry";
-        break;
-    case "50":
-        ruler.innerText = "Kazimierz III Wielki";
-        break;
-    case "100":
-        ruler.innerText = "Władysław II Jagiełło";
-        break;
-    case "200":
-        ruler.innerText = "Zygmunt I Stary";
-        break;
-    case "500":
-        ruler.innerText = "Jan III Sobieski";
-        break;
-    default:
-        ruler.innerText = "Taki banknot nie istnieje";
-};
